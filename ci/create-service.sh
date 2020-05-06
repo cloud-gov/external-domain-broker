dec086dea115d9b01a841960b865c8abeeda0d73
Tammer Saleh <tammer@superorbit.al>
2020-05-06 19:56:19 +0000

    Unique ci service key

--- ci/create-service.sh
+++ ci/create-service.sh
@@ -7,14 +7,16 @@ cf api "$CF_API_URL"
 (set +x; cf auth "$CF_USERNAME" "$CF_PASSWORD")
 cf target -o "$CF_ORGANIZATION" -s "$CF_SPACE"
 
+KEY="${SVC_NAME}-ci-key"
+
 if [[ -n "$(cf service "$SVC_NAME" --guid)" ]]; then
   cf create-service "$SVC_SERVICE" "$SVC_PLAN" "$SVC_NAME"
 fi
 
 echo -n "Waiting for service instance..."
-while ! cf create-service-key "$SVC_NAME" temp-ci-key > /dev/null 2>&1; do
+while ! cf create-service-key "$SVC_NAME" "$KEY" > /dev/null 2>&1; do
   echo -n "."
   sleep 5
 done
 echo
-cf delete-service-key -f "$SVC_NAME" temp-ci-key || true
+cf delete-service-key -f "$SVC_NAME" "$KEY" || true
