fe8c9488367615d5f8f5a5868ff2e3b8b5ddc1a2
Tammer Saleh <tammer@superorbit.al>
2020-05-06 19:29:08 +0000

    fix pipeline some more

--- ci/upgrade-schemas.sh
+++ ci/upgrade-schemas.sh
@@ -10,7 +10,7 @@ cf target -o "$CF_ORGANIZATION" -s "$CF_SPACE"
 
 # dummy app so we can run a task.
 cf push \
-  -f src/manifest.yml \
+  -f src/manifests/app.yml \
   -p src \
   -i 1 \
   --var DB_NAME="$DB_NAME" \
