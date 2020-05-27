48492ed7c02f753a0e5310f9e2d715a2822ab6dd
Ben Berry <ben.berry@gsa.gov>
2020-05-27 16:52:45 -0700

    one more flask_app update

--- ci/upgrade-schema.sh
+++ ci/upgrade-schema.sh
@@ -25,7 +25,7 @@ cf push \
 # This is just to put logs in the concourse output.
 (cf logs "$APP_NAME" | grep "TASK/db-upgrade") &
 
-cmd="FLASK_APP=broker.app:create_app() flask db upgrade"
+cmd="FLASK_APP='broker.app:create_app()' flask db upgrade"
 id=$(cf run-task "$APP_NAME" "$cmd" --name="db-upgrade" | grep "task id:" | awk '{print $3}')
 
 set +x
