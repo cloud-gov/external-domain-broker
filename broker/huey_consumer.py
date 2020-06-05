02cfc44ae544ca706fb46b7d9b3495067978f6e1
Ben Berry <ben.berry@gsa.gov>
2020-06-05 14:51:20 -0700

    give huey access to the app context

--- broker/huey_consumer.py
+++ broker/huey_consumer.py
@@ -1,6 +1,12 @@
 from sap import cf_logging
-from broker.tasks import pipelines  # noqa F401
-from broker.tasks.huey import huey  # noqa F401
+
+from broker.app import create_app
 
 if not cf_logging._SETUP_DONE:
     cf_logging.init()
+
+app = create_app()
+
+with app.app_context():
+    from broker.tasks import pipelines  # noqa F401
+    from broker.tasks.huey import huey  # noqa F401
