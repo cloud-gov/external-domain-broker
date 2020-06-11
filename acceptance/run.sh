90b53754e6c637bc8e18d3c3e8bf9ece24993057
Ben Berry <ben.berry@gsa.gov>
2020-06-11 14:01:36 -0700

    fix bash syntax

--- acceptance/run.sh
+++ acceptance/run.sh
@@ -113,7 +113,7 @@ EOF
     --change-batch file://./create-cname.json > changeinfo.json
   change_id=$(cat changeinfo.json | jq -r '.ChangeInfo.Id')
   change=$(cat changeinfo.json | jq -r '.ChangeInfo.Status')
-  while [[ $change =~ PENDING]]; do 
+  while [[ $change =~ PENDING ]]; do 
     sleep 60
     change=$(aws route53 | jq -r '.ChangeInfo.Status')
   done
