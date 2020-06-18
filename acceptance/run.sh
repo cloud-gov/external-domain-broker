aa4ab85179b7953d3290fd1ff734e7064b1ea75c
Ben Berry <ben.berry@gsa.gov>
2020-06-18 12:07:50 -0700

    give the app a second to wake up

--- acceptance/run.sh
+++ acceptance/run.sh
@@ -157,6 +157,7 @@ tests() {
       --var "domain_1=$DOMAIN_1" \
       --var "name=$INSTANCE"
   )
+  sleep 30
 
   echo "Testing the test application through the domains via HTTPS."
   curl -sSL "https://$DOMAIN_0" | grep "ALIVE"
