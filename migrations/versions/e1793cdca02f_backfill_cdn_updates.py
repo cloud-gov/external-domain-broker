853e6ce3f8d37f83a3e5703875424262aaeb20e8
Ben Berry <ben.berry@gsa.gov>
2020-06-29 18:07:46 -0700

    guard migrations

--- migrations/versions/e1793cdca02f_backfill_cdn_updates.py
+++ migrations/versions/e1793cdca02f_backfill_cdn_updates.py
@@ -37,8 +37,10 @@ def upgrade():
 
     for service_instance in session.query(ServiceInstance):
         if service_instance.instance_type == "cdn_service_instance":
-            service_instance.origin_protocol_policy = 'https-only'
-            service_instance.forwarded_headers = ['HOST']
+            if service_instance.origin_protocol_policy is None:
+                service_instance.origin_protocol_policy = 'https-only'
+            if service_instance.forwarded_headers == [] or service_instance.forwarded_headers is None:
+                service_instance.forwarded_headers = ['HOST']
             session.add(service_instance)
     session.commit()
 
