246d3731064e2b82a76f3b7c7184e6fceff2763e
Ben Berry <ben.berry@gsa.gov>
2020-06-30 14:51:28 -0700

    mark operations from failed pipelines as failed

---
+++ tests/integration/test_failing_tasks.py
@@ -0,0 +1,63 @@
+import time
+import pytest
+
+from huey.exceptions import TaskException
+from broker.extensions import config, db
+from broker.tasks.huey import huey
+from broker.tasks.letsencrypt import initiate_challenges
+from broker.models import Operation
+
+from tests.lib.factories import OperationFactory
+from tests.lib.tasks import fallible_huey, immediate_huey
+
+
+def test_operations_marked_failed_after_failing(tasks, clean_db):
+    @huey.task()
+    def no_retry_task(operation_id):
+        raise Exception()
+
+    no_retries_left_operation = OperationFactory.create(
+        id="9876",
+        state="InProgress",
+        action="Provision",
+        service_instance_id="nonextistent",
+    )
+    db.session.commit()
+    with fallible_huey() as h:
+        with immediate_huey() as h:
+            # this task should fail because we have not created a user
+            task = no_retry_task("9876")
+            with pytest.raises(TaskException):
+                result = task()
+    no_retries_left_operation = Operation.query.get("9876")
+    assert no_retries_left_operation.state == "failed"
+
+
+def test_retry_tasks_marked_failed_only_after_last_retry():
+    global retry_marked_failed
+    retry_marked_failed = False
+
+    @huey.task(retries=7)
+    def retry_task(operation_id):
+        operation = Operation.query.get(operation_id)
+        global retry_marked_failed
+        if operation.state == "failed":
+            retry_marked_failed = True
+        raise Exception()
+
+    operation_with_retries = OperationFactory.create(
+        id="6789",
+        state="InProgress",
+        action="Provision",
+        service_instance_id="nonextistent",
+    )
+    db.session.commit()
+    with fallible_huey() as h:
+        with immediate_huey() as h:
+            # this task should fail because we have not created a user
+            task = retry_task("6789")
+            with pytest.raises(TaskException):
+                result = task()
+    operation_with_retries = Operation.query.get("6789")
+    assert operation_with_retries.state == "failed"
+    assert not retry_marked_failed
