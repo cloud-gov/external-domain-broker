e55e54bb6048129292a9a382aead45a0dbb696c2
Ben Berry <ben.berry@gsa.gov>
2020-08-11 12:28:13 -0700

    Drop built-in errorhandlers, since they're leaky

--- broker/app.py
+++ broker/app.py
@@ -1,7 +1,11 @@
+from http import HTTPStatus
 import logging
 
 from flask import Flask
 from openbrokerapi import api as openbrokerapi
+from openbrokerapi.helper import to_json_response
+from openbrokerapi.response import ErrorResponse
+
 from sap import cf_logging
 from sap.cf_logging import flask_logging
 
@@ -27,10 +31,30 @@ def create_app():
     )
 
     app.register_blueprint(openbrokerapi.get_blueprint(API(), credentials, logger))
+    del app.error_handler_spec["open_broker"][None][Exception]
+    del app.error_handler_spec["open_broker"][None][NotImplementedError]
 
     # Endpoint to test if server comes up
     @app.route("/ping")
     def ping():
         return "PONG"
+
+    @app.errorhandler(Exception)
+    def handle_base_exception(e):
+        logger.exception(e)
+        return (
+            to_json_response(
+                ErrorResponse(description="Unhandled error during request")
+            ),
+            HTTPStatus.INTERNAL_SERVER_ERROR,
+        )
+
+    @app.errorhandler(NotImplementedError)
+    def handle_not_implemented(e):
+        logger.exception(e)
+        return (
+            to_json_response(ErrorResponse(description="Not Implemented")),
+            HTTPStatus.NOT_IMPLEMENTED,
+        )
 
     return app
