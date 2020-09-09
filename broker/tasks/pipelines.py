4421881e7bcd8c08e2e2bf33250db8074fa170d8
Ben Berry <ben.berry@gsa.gov>
2020-09-09 15:38:54 -0700

    refactor correlation_id to tidy up function calls

--- broker/tasks/pipelines.py
+++ broker/tasks/pipelines.py
@@ -11,34 +11,21 @@ def queue_all_alb_provision_tasks_for_operation(operation_id: int, correlation_i
         raise RuntimeError("correlation_id must be set")
     if operation_id is None:
         raise RuntimeError("operation_id must be set")
+    correlation = {"correlation_id": correlation_id}
     task_pipeline = (
-        letsencrypt.create_user.s(operation_id, correlation_id=correlation_id)
-        .then(
-            letsencrypt.generate_private_key,
-            operation_id,
-            correlation_id=correlation_id,
-        )
-        .then(
-            letsencrypt.initiate_challenges, operation_id, correlation_id=correlation_id
-        )
-        .then(route53.create_TXT_records, operation_id, correlation_id=correlation_id)
-        .then(route53.wait_for_changes, operation_id, correlation_id=correlation_id)
-        .then(
-            letsencrypt.answer_challenges, operation_id, correlation_id=correlation_id
-        )
-        .then(
-            letsencrypt.retrieve_certificate,
-            operation_id,
-            correlation_id=correlation_id,
-        )
-        .then(
-            iam.upload_server_certificate, operation_id, correlation_id=correlation_id
-        )
-        .then(alb.select_alb, operation_id, correlation_id=correlation_id)
-        .then(alb.add_certificate_to_alb, operation_id, correlation_id=correlation_id)
-        .then(route53.create_ALIAS_records, operation_id, correlation_id=correlation_id)
-        .then(route53.wait_for_changes, operation_id, correlation_id=correlation_id)
-        .then(update_operations.provision, operation_id, correlation_id=correlation_id)
+        letsencrypt.create_user.s(operation_id, **correlation)
+        .then(letsencrypt.generate_private_key, operation_id, **correlation)
+        .then(letsencrypt.initiate_challenges, operation_id, **correlation)
+        .then(route53.create_TXT_records, operation_id, **correlation)
+        .then(route53.wait_for_changes, operation_id, **correlation)
+        .then(letsencrypt.answer_challenges, operation_id, **correlation)
+        .then(letsencrypt.retrieve_certificate, operation_id, **correlation)
+        .then(iam.upload_server_certificate, operation_id, **correlation)
+        .then(alb.select_alb, operation_id, **correlation)
+        .then(alb.add_certificate_to_alb, operation_id, **correlation)
+        .then(route53.create_ALIAS_records, operation_id, **correlation)
+        .then(route53.wait_for_changes, operation_id, **correlation)
+        .then(update_operations.provision, operation_id, **correlation)
     )
     huey.enqueue(task_pipeline)
 
@@ -50,21 +37,14 @@ def queue_all_alb_deprovision_tasks_for_operation(
         raise RuntimeError("correlation_id must be set")
     if operation_id is None:
         raise RuntimeError("operation_id must be set")
+    correlation = {"correlation_id": correlation_id}
     task_pipeline = (
-        update_operations.cancel_pending_provisioning.s(
-            operation_id, correlation_id=correlation_id
-        )
-        .then(route53.remove_ALIAS_records, operation_id, correlation_id=correlation_id)
-        .then(route53.remove_TXT_records, operation_id, correlation_id=correlation_id)
-        .then(
-            alb.remove_certificate_from_alb, operation_id, correlation_id=correlation_id
-        )
-        .then(
-            iam.delete_server_certificate, operation_id, correlation_id=correlation_id
-        )
-        .then(
-            update_operations.deprovision, operation_id, correlation_id=correlation_id
-        )
+        update_operations.cancel_pending_provisioning.s(operation_id, **correlation)
+        .then(route53.remove_ALIAS_records, operation_id, **correlation)
+        .then(route53.remove_TXT_records, operation_id, **correlation)
+        .then(alb.remove_certificate_from_alb, operation_id, **correlation)
+        .then(iam.delete_server_certificate, operation_id, **correlation)
+        .then(update_operations.deprovision, operation_id, **correlation)
     )
     huey.enqueue(task_pipeline)
 
@@ -74,40 +54,21 @@ def queue_all_cdn_provision_tasks_for_operation(operation_id: int, correlation_i
         raise RuntimeError("correlation_id must be set")
     if operation_id is None:
         raise RuntimeError("operation_id must be set")
+    correlation = {"correlation_id": correlation_id}
     task_pipeline = (
-        letsencrypt.create_user.s(operation_id, correlation_id=correlation_id)
-        .then(
-            letsencrypt.generate_private_key,
-            operation_id,
-            correlation_id=correlation_id,
-        )
-        .then(
-            letsencrypt.initiate_challenges, operation_id, correlation_id=correlation_id
-        )
-        .then(route53.create_TXT_records, operation_id, correlation_id=correlation_id)
-        .then(route53.wait_for_changes, operation_id, correlation_id=correlation_id)
-        .then(
-            letsencrypt.answer_challenges, operation_id, correlation_id=correlation_id
-        )
-        .then(
-            letsencrypt.retrieve_certificate,
-            operation_id,
-            correlation_id=correlation_id,
-        )
-        .then(
-            iam.upload_server_certificate, operation_id, correlation_id=correlation_id
-        )
-        .then(
-            cloudfront.create_distribution, operation_id, correlation_id=correlation_id
-        )
-        .then(
-            cloudfront.wait_for_distribution,
-            operation_id,
-            correlation_id=correlation_id,
-        )
-        .then(route53.create_ALIAS_records, operation_id, correlation_id=correlation_id)
-        .then(route53.wait_for_changes, operation_id, correlation_id=correlation_id)
-        .then(update_operations.provision, operation_id, correlation_id=correlation_id)
+        letsencrypt.create_user.s(operation_id, **correlation)
+        .then(letsencrypt.generate_private_key, operation_id, **correlation)
+        .then(letsencrypt.initiate_challenges, operation_id, **correlation)
+        .then(route53.create_TXT_records, operation_id, **correlation)
+        .then(route53.wait_for_changes, operation_id, **correlation)
+        .then(letsencrypt.answer_challenges, operation_id, **correlation)
+        .then(letsencrypt.retrieve_certificate, operation_id, **correlation)
+        .then(iam.upload_server_certificate, operation_id, **correlation)
+        .then(cloudfront.create_distribution, operation_id, **correlation)
+        .then(cloudfront.wait_for_distribution, operation_id, **correlation)
+        .then(route53.create_ALIAS_records, operation_id, **correlation)
+        .then(route53.wait_for_changes, operation_id, **correlation)
+        .then(update_operations.provision, operation_id, **correlation)
     )
     huey.enqueue(task_pipeline)
 
@@ -119,31 +80,16 @@ def queue_all_cdn_deprovision_tasks_for_operation(
         raise RuntimeError("correlation_id must be set")
     if operation_id is None:
         raise RuntimeError("operation_id must be set")
+    correlation = {"correlation_id": correlation_id}
     task_pipeline = (
-        update_operations.cancel_pending_provisioning.s(
-            operation_id, correlation_id=correlation_id
-        )
-        .then(route53.remove_ALIAS_records, operation_id, correlation_id=correlation_id)
-        .then(route53.remove_TXT_records, operation_id, correlation_id=correlation_id)
-        .then(
-            cloudfront.disable_distribution, operation_id, correlation_id=correlation_id
-        )
-        .then(
-            cloudfront.wait_for_distribution_disabled,
-            operation_id,
-            correlation_id=correlation_id,
-        )
-        .then(
-            cloudfront.delete_distribution,
-            operation_id=operation_id,
-            correlation_id=correlation_id,
-        )
-        .then(
-            iam.delete_server_certificate, operation_id, correlation_id=correlation_id
-        )
-        .then(
-            update_operations.deprovision, operation_id, correlation_id=correlation_id
-        )
+        update_operations.cancel_pending_provisioning.s(operation_id, **correlation)
+        .then(route53.remove_ALIAS_records, operation_id, **correlation)
+        .then(route53.remove_TXT_records, operation_id, **correlation)
+        .then(cloudfront.disable_distribution, operation_id, **correlation)
+        .then(cloudfront.wait_for_distribution_disabled, operation_id, **correlation)
+        .then(cloudfront.delete_distribution, operation_id=operation_id, **correlation)
+        .then(iam.delete_server_certificate, operation_id, **correlation)
+        .then(update_operations.deprovision, operation_id, **correlation)
     )
     huey.enqueue(task_pipeline)
 
@@ -187,108 +133,55 @@ def queue_all_cdn_renewal_tasks_for_service_instance(operation_id, **kwargs):
 
 
 def queue_all_cdn_update_tasks_for_service_instance(operation_id, correlation_id):
+    correlation = {"correlation_id": correlation_id}
     task_pipeline = (
-        letsencrypt.initiate_challenges.s(operation_id, correlation_id=correlation_id)
-        .then(route53.create_TXT_records, operation_id, correlation_id=correlation_id)
-        .then(route53.wait_for_changes, operation_id, correlation_id=correlation_id)
-        .then(
-            letsencrypt.answer_challenges, operation_id, correlation_id=correlation_id
-        )
-        .then(
-            letsencrypt.retrieve_certificate,
-            operation_id,
-            correlation_id=correlation_id,
-        )
-        .then(
-            iam.upload_server_certificate, operation_id, correlation_id=correlation_id
-        )
-        .then(
-            cloudfront.update_distribution, operation_id, correlation_id=correlation_id
-        )
-        .then(
-            cloudfront.wait_for_distribution,
-            operation_id,
-            correlation_id=correlation_id,
-        )
-        .then(update_operations.provision, operation_id, correlation_id=correlation_id)
+        letsencrypt.initiate_challenges.s(operation_id, **correlation)
+        .then(route53.create_TXT_records, operation_id, **correlation)
+        .then(route53.wait_for_changes, operation_id, **correlation)
+        .then(letsencrypt.answer_challenges, operation_id, **correlation)
+        .then(letsencrypt.retrieve_certificate, operation_id, **correlation)
+        .then(iam.upload_server_certificate, operation_id, **correlation)
+        .then(cloudfront.update_distribution, operation_id, **correlation)
+        .then(cloudfront.wait_for_distribution, operation_id, **correlation)
+        .then(update_operations.provision, operation_id, **correlation)
     )
     huey.enqueue(task_pipeline)
 
 
 def queue_all_cdn_update_tasks_for_operation(operation_id, correlation_id):
+    correlation = {"correlation_id": correlation_id}
     task_pipeline = (
-        letsencrypt.generate_private_key.s(operation_id, correlation_id=correlation_id)
-        .then(
-            letsencrypt.initiate_challenges, operation_id, correlation_id=correlation_id
-        )
-        .then(route53.create_TXT_records, operation_id, correlation_id=correlation_id)
-        .then(route53.wait_for_changes, operation_id, correlation_id=correlation_id)
-        .then(
-            letsencrypt.answer_challenges, operation_id, correlation_id=correlation_id
-        )
-        .then(
-            letsencrypt.retrieve_certificate,
-            operation_id,
-            correlation_id=correlation_id,
-        )
-        .then(
-            iam.upload_server_certificate, operation_id, correlation_id=correlation_id
-        )
-        .then(
-            cloudfront.update_distribution, operation_id, correlation_id=correlation_id
-        )
-        .then(
-            cloudfront.wait_for_distribution,
-            operation_id,
-            correlation_id=correlation_id,
-        )
-        .then(
-            iam.delete_previous_server_certificate,
-            operation_id,
-            correlation_id=correlation_id,
-        )
-        .then(
-            update_operations.update_complete,
-            operation_id,
-            correlation_id=correlation_id,
-        )
+        letsencrypt.generate_private_key.s(operation_id, **correlation)
+        .then(letsencrypt.initiate_challenges, operation_id, **correlation)
+        .then(route53.create_TXT_records, operation_id, **correlation)
+        .then(route53.wait_for_changes, operation_id, **correlation)
+        .then(letsencrypt.answer_challenges, operation_id, **correlation)
+        .then(letsencrypt.retrieve_certificate, operation_id, **correlation)
+        .then(iam.upload_server_certificate, operation_id, **correlation)
+        .then(cloudfront.update_distribution, operation_id, **correlation)
+        .then(cloudfront.wait_for_distribution, operation_id, **correlation)
+        .then(iam.delete_previous_server_certificate, operation_id, **correlation)
+        .then(update_operations.update_complete, operation_id, **correlation)
     )
     huey.enqueue(task_pipeline)
 
 
 def queue_all_alb_update_tasks_for_operation(operation_id, correlation_id):
+    correlation = {"correlation_id": correlation_id}
     task_pipeline = (
-        letsencrypt.generate_private_key.s(operation_id, correlation_id=correlation_id)
-        .then(
-            letsencrypt.initiate_challenges, operation_id, correlation_id=correlation_id
-        )
-        .then(route53.create_TXT_records, operation_id, correlation_id=correlation_id)
-        .then(route53.wait_for_changes, operation_id, correlation_id=correlation_id)
-        .then(
-            letsencrypt.answer_challenges, operation_id, correlation_id=correlation_id
-        )
-        .then(
-            letsencrypt.retrieve_certificate,
-            operation_id,
-            correlation_id=correlation_id,
-        )
-        .then(
-            iam.upload_server_certificate, operation_id, correlation_id=correlation_id
-        )
-        .then(alb.select_alb, operation_id, correlation_id=correlation_id)
-        .then(alb.add_certificate_to_alb, operation_id, correlation_id=correlation_id)
-        .then(route53.create_ALIAS_records, operation_id, correlation_id=correlation_id)
-        .then(route53.wait_for_changes, operation_id, correlation_id=correlation_id)
-        .then(
-            alb.remove_certificate_from_previous_alb,
-            operation_id,
-            correlation_id=correlation_id,
-        )
-        .then(
-            iam.delete_previous_server_certificate,
-            operation_id,
-            correlation_id=correlation_id,
-        )
-        .then(update_operations.provision, operation_id, correlation_id=correlation_id)
+        letsencrypt.generate_private_key.s(operation_id, **correlation)
+        .then(letsencrypt.initiate_challenges, operation_id, **correlation)
+        .then(route53.create_TXT_records, operation_id, **correlation)
+        .then(route53.wait_for_changes, operation_id, **correlation)
+        .then(letsencrypt.answer_challenges, operation_id, **correlation)
+        .then(letsencrypt.retrieve_certificate, operation_id, **correlation)
+        .then(iam.upload_server_certificate, operation_id, **correlation)
+        .then(alb.select_alb, operation_id, **correlation)
+        .then(alb.add_certificate_to_alb, operation_id, **correlation)
+        .then(route53.create_ALIAS_records, operation_id, **correlation)
+        .then(route53.wait_for_changes, operation_id, **correlation)
+        .then(alb.remove_certificate_from_previous_alb, operation_id, **correlation)
+        .then(iam.delete_previous_server_certificate, operation_id, **correlation)
+        .then(update_operations.provision, operation_id, **correlation)
     )
     huey.enqueue(task_pipeline)
