62934d0d00444b6d75a336537fb4646679aaec76
Ben Berry <ben.berry@gsa.gov>
2020-10-01 11:35:22 -0700

    Allow arrays for domains parameter
    We've had a few users bitten by this - arrays are the obvious choice for
    the domain parameter, but we went with comma-separated strings initially
    to be consistent with the old brokers. This change allows either

---
+++ tests/unit/test_provision.py
@@ -0,0 +1,15 @@
+import pytest
+from broker import api
+
+
+def test_parse_domains():
+    assert api.parse_domain_options(dict(domains="example.com")) == ["example.com"]
+    assert api.parse_domain_options(dict(domains="example.com,example.gov")) == [
+        "example.com",
+        "example.gov",
+    ]
+    assert api.parse_domain_options(dict(domains=["example.com"])) == ["example.com"]
+    assert api.parse_domain_options(dict(domains=["example.com", "example.gov"])) == [
+        "example.com",
+        "example.gov",
+    ]
