7dff8976c1bda1b59993162c1b686d89621faf87
Ben Berry <ben.berry@gsa.gov>
2021-07-13 08:32:44 -0700

    Allow updating migrator instances to alb instances
    Add pipeline for migrator -> alb updates
    Change validators to allow updating migrator -> alb
    Add API steps to prep migrator for update to alb

--- broker/api.py
+++ broker/api.py
@@ -44,6 +44,7 @@ from broker.tasks.pipelines import (
     queue_all_cdn_provision_tasks_for_operation,
     queue_all_cdn_update_tasks_for_operation,
     queue_all_cdn_broker_migration_tasks_for_operation,
+    queue_all_domain_broker_migration_tasks_for_operation,
 )
 
 ALB_PLAN_ID = "6f60835c-8964-4f1f-a19a-579fb27ce694"
@@ -317,6 +318,15 @@ class API(ServiceBroker):
                 update_cdn_params_for_migration(instance, params)
                 db.session.add(instance.current_certificate)
                 queue = queue_all_cdn_broker_migration_tasks_for_operation
+            elif details.plan_id == ALB_PLAN_ID:
+                noop = False
+                validate_migration_to_alb_params(params)
+                instance = change_instance_type(
+                    instance, ALBServiceInstance, db.session
+                )
+                update_alb_params_for_migration(instance, params)
+                db.session.add(instance.current_certificate)
+                queue = queue_all_domain_broker_migration_tasks_for_operation
             else:
                 raise ClientError("Updating to this service plan is not supported")
         if noop:
@@ -455,6 +465,40 @@ def validate_migration_to_cdn_params(params):
         # fair and smart to require all params
         if not param in params:
             raise ClientError(f"Missing parameter {param}")
+
+
+def validate_migration_to_alb_params(params):
+    required = [
+        "iam_server_certificate_name",
+        "iam_server_certificate_id",
+        "iam_server_certificate_arn",
+        "domain_internal",
+        "alb_arn",
+        "alb_listener_arn",
+        "hosted_zone_id",
+    ]
+    for param in required:
+        # since this should only be hit by another app, it seems
+        # fair and smart to require all params
+        if not param in params:
+            raise ClientError(f"Missing parameter {param}")
+
+
+def update_alb_params_for_migration(instance, params):
+    instance.current_certificate = Certificate(service_instance_id=instance.id)
+    instance.current_certificate.iam_server_certificate_id = params[
+        "iam_server_certificate_id"
+    ]
+    instance.current_certificate.iam_server_certificate_arn = params[
+        "iam_server_certificate_arn"
+    ]
+    instance.current_certificate.iam_server_certificate_name = params[
+        "iam_server_certificate_name"
+    ]
+    instance.domain_internal = params["domain_internal"]
+    instance.route53_alias_hosted_zone = params["hosted_zone_id"]
+    instance.alb_listener_arn = params["alb_listener_arn"]
+    instance.alb_arn = params["alb_arn"]
 
 
 def update_cdn_params_for_migration(instance, params):
--- broker/tasks/pipelines.py
+++ broker/tasks/pipelines.py
@@ -195,3 +195,28 @@ def queue_all_cdn_broker_migration_tasks_for_operation(operation_id, correlation
         .then(update_operations.provision, operation_id, **correlation)
     )
     huey.enqueue(task_pipeline)
+
+
+def queue_all_domain_broker_migration_tasks_for_operation(operation_id, correlation_id):
+    correlation = {"correlation_id": correlation_id}
+    task_pipeline = (
+        letsencrypt.create_user.s(operation_id, **correlation)
+        .then(letsencrypt.generate_private_key, operation_id, **correlation)
+        .then(letsencrypt.initiate_challenges, operation_id, **correlation)
+        # create alias records here is probably not necessary, but belt + suspenders
+        .then(route53.create_ALIAS_records, operation_id, **correlation)
+        .then(route53.wait_for_changes, operation_id, **correlation)
+        .then(route53.create_TXT_records, operation_id, **correlation)
+        .then(route53.wait_for_changes, operation_id, **correlation)
+        .then(letsencrypt.answer_challenges, operation_id, **correlation)
+        .then(letsencrypt.retrieve_certificate, operation_id, **correlation)
+        .then(iam.upload_server_certificate, operation_id, **correlation)
+        .then(alb.select_alb, operation_id, **correlation)
+        .then(alb.add_certificate_to_alb, operation_id, **correlation)
+        .then(route53.create_ALIAS_records, operation_id, **correlation)
+        .then(route53.wait_for_changes, operation_id, **correlation)
+        .then(alb.remove_certificate_from_previous_alb, operation_id, **correlation)
+        .then(iam.delete_previous_server_certificate, operation_id, **correlation)
+        .then(update_operations.provision, operation_id, **correlation)
+    )
+    huey.enqueue(task_pipeline)
