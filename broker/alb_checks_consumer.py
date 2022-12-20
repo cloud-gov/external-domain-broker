018edac53328fe3c05fd0fb21866dfc31ea3478d
Mark Boyd <mark.boyd@gsa.gov>
2022-12-20 11:41:14 -0500

    update pipeline

--- broker/alb_checks_consumer.py
+++ broker/alb_checks_consumer.py
@@ -1,4 +1,5 @@
 from broker.app import create_app
 from broker.tasks.huey import huey  # noqa F401
+from broker.tasks import alb_checks
 
 huey.immediate = True
---
+++ ci/check-duplicate-certs.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+shopt -s inherit_errexit
+
+export PYTHONPATH=$(dirname "$0")/..
+
+# send logs to dev null, since we create other log handlers elsewhere
+exec broker/alb_checks_consumer.py "$@" broker.alb_checks_consumer.huey
