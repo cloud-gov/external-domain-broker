78d350c44a0798603d103b64ca8fea27d922aa6c
Mark Boyd <mark.boyd@gsa.gov>
2022-12-20 15:54:33 -0500

    try using venv

--- ci/check-duplicate-certs.sh
+++ ci/check-duplicate-certs.sh
@@ -3,13 +3,17 @@
 set -euo pipefail
 shopt -s inherit_errexit
 
+python -m venv venv
+source ./venv/bin/activate
+
+python --version
+
 python -m pip install -r src2/requirements.txt
 
 # python src2/broker/alb_checks_consumer.py "$@" broker.alb_checks_consumer.huey
 export DUPLICATE_CERT_METRICS_FILEPATH=$(mktemp)
 
-SRCPATH=$(dirname "$0")/..
-export PYTHONPATH="$PYTHONPATH:$SRCPATH"
+export PYTHONPATH=$(dirname "$0")/..
 
 python src2/broker/alb_checks.py
 
