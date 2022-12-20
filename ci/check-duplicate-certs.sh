9aaa13bc38e7b5cb4a269a18b6a65c2b90788bf0
Mark Boyd <mark.boyd@gsa.gov>
2022-12-20 15:46:33 -0500

    another fix

--- ci/check-duplicate-certs.sh
+++ ci/check-duplicate-certs.sh
@@ -8,7 +8,8 @@ python -m pip install -r src2/requirements.txt
 # python src2/broker/alb_checks_consumer.py "$@" broker.alb_checks_consumer.huey
 export DUPLICATE_CERT_METRICS_FILEPATH=$(mktemp)
 
-export PYTHONPATH=$(dirname "$0")/..
+SRCPATH=$(dirname "$0")/..
+export PYTHONPATH="$PYTHONPATH:$SRCPATH"
 
 python src2/broker/alb_checks.py
 
