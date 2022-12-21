75060a1d3baa4024c9f1436bec33342d88241e3e
Mark Boyd <mark.boyd@gsa.gov>
2022-12-21 15:50:56 -0500

    code to get matching listener arns

--- broker/check_duplicate_certs.py
+++ broker/check_duplicate_certs.py
@@ -2,7 +2,8 @@ import logging
 
 from sqlalchemy import func, select, desc
 
-from broker.extensions import db
+from broker.aws import alb
+from broker.extensions import config, db
 from broker.models import ALBServiceInstance, Certificate
 
 logger = logging.getLogger(__name__)
@@ -46,11 +47,32 @@ def delete_duplicate_cert_db_record(duplicate_cert):
         Certificate.id == duplicate_cert.id
     ).delete()
 
+def get_matching_alb_listener_arns_for_cert_arns(duplicate_cert_arns):
+    matched_listeners_dict = {}
+    all_matched_cert_arns = []
+    for listener_arn in config.ALB_LISTENER_ARNS:
+        response = alb.describe_listener_certificates(
+            ListenerArn=listener_arn,
+        )
+        listener_cert_arns = [cert["CertificateArn"] for cert in response["Certificates"]]
+        # Get list of duplicate cert ARNs that were matched for this ALB listener ARN
+        matched_cert_arns = list(set(listener_cert_arns) & set(duplicate_cert_arns))
+
+        if len(matched_cert_arns) > 0:
+            # Update dict of cert ARNs to ALB listener ARNs
+            matched_listeners_dict.update(dict(zip(matched_cert_arns, [listener_arn] * len(matched_cert_arns))))
+            all_matched_cert_arns = all_matched_cert_arns + matched_cert_arns
+            # We have matched all the duplicate cert ARNs, so break out of loop
+            if len(all_matched_cert_arns) == len(duplicate_cert_arns):
+                break
+    return matched_listeners_dict
+
 def fix_duplicate_alb_certs():
   for duplicate_result in find_duplicate_alb_certs():
     [service_instance_id, num_duplicates] = duplicate_result
     duplicate_certs = get_duplicate_certs_for_service(service_instance_id)
     logger.info(f"Found {num_duplicates} duplicate certificates for service instance {service_instance_id}")
+    duplicate_cert_arns = [cert.iam_server_certificate.arn for cert in duplicate_certs]
     for duplicate_cert in duplicate_certs:
         delete_duplicate_cert_db_record(duplicate_cert)
     
--- tests/integration/alb/test_alb_fix_duplicate_certs.py
+++ tests/integration/alb/test_alb_fix_duplicate_certs.py
@@ -5,7 +5,7 @@ from tests.lib.factories import (
     ALBServiceInstanceFactory,
 )
 
-from broker.check_duplicate_certs import get_duplicate_certs_for_service, fix_duplicate_alb_certs
+from broker.check_duplicate_certs import get_duplicate_certs_for_service, fix_duplicate_alb_certs, get_matching_alb_listener_arns_for_cert_arns
 
 def test_get_duplicate_certs_for_service(no_context_clean_db, no_context_app):
   with no_context_app.app_context():
@@ -52,3 +52,33 @@ def test_fix_duplicate_certs_for_service(no_context_clean_db, no_context_app):
     
     results = get_duplicate_certs_for_service(service_instance.id)
     assert len(results) == 0
+
+def test_get_matching_alb_listener_arns_for_single_cert_arn(alb):
+    alb.expect_get_certificates_for_listener("listener-arn-0", 1)
+    results = get_matching_alb_listener_arns_for_cert_arns(["certificate-arn"])
+    assert results == {
+      "certificate-arn": "listener-arn-0"
+    }
+
+def test_get_matching_alb_listener_arns_for_multiple_cert_arns(alb):
+    alb.expect_get_certificates_for_listener("listener-arn-0", 1)
+    results = get_matching_alb_listener_arns_for_cert_arns([
+      "certificate-arn",
+      "certificate-arn-0"
+    ])
+    assert results == {
+      "certificate-arn": "listener-arn-0",
+      "certificate-arn-0": "listener-arn-0"
+    }
+
+# def test_get_matching_alb_listener_arns_for_multiple_listeners(alb):
+#     alb.expect_get_certificates_for_listener("listener-arn-0", 1)
+#     alb.expect_get_certificates_for_listener("listener-arn-1", 2)
+#     results = get_matching_alb_listener_arns_for_cert_arns([
+#       "certificate-arn",
+#       "certificate-arn-0"
+#     ])
+#     assert results == {
+#       "certificate-arn": "listener-arn-0",
+#       "certificate-arn-0": "listener-arn-0"
+#     }
