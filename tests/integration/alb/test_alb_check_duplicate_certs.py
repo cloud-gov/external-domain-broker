54a97b1d3444461f7b1df11db33540780daabe6d
Mark Boyd <mark.boyd@gsa.gov>
2023-01-30 15:32:55 -0500

    revert debugging changes

--- tests/integration/alb/test_alb_check_duplicate_certs.py
+++ tests/integration/alb/test_alb_check_duplicate_certs.py
@@ -8,7 +8,8 @@ from tests.lib.factories import (
 from broker.duplicate_certs import (
   find_duplicate_alb_certs,
   log_duplicate_alb_cert_metrics,
-  get_service_duplicate_alb_cert_count
+  get_service_duplicate_alb_cert_count,
+  get_and_log_service_duplicate_alb_cert_metric
 )
 
 def test_no_duplicate_alb_certs(no_context_clean_db, no_context_app):
@@ -88,6 +89,32 @@ def test_service_duplicate_alb_certs(no_context_clean_db, no_context_app):
     no_context_clean_db.session.commit()
 
     assert(get_service_duplicate_alb_cert_count(service_instance.id) == 1)
+
+
+def test_service_duplicate_alb_certs_output(no_context_clean_db, no_context_app):
+  with no_context_app.app_context():
+    service_instance = ALBServiceInstanceFactory.create(id="1234")
+    certificate = CertificateFactory.create(
+        service_instance=service_instance,
+    )
+    CertificateFactory.create(
+        service_instance=service_instance,
+    )
+    service_instance.current_certificate_id = certificate.id
+
+    no_context_clean_db.session.commit()
+
+    class FakeLogger:
+      def __init__(self):
+        self.output = ""
+
+      def info(self, input):
+        self.output = self.output + input
+    fakeLogger = FakeLogger()
+
+    get_and_log_service_duplicate_alb_cert_metric(service_instance.id, logger=fakeLogger)
+
+    assert fakeLogger.output.strip() == "service_instance_duplicate_cert_count{service_instance_id=\"1234\"} 1"
 
 def test_duplicate_alb_certs_output(no_context_clean_db, no_context_app):
   with no_context_app.app_context():
