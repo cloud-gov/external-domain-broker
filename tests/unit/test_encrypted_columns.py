85fd726529cf7e57e4109f1206acd29c8c6d4231
Mark Boyd <mark.boyd@gsa.gov>
2024-02-07 15:12:24 -0500

    update tests for compatibility with sqlachemy 2.0

--- tests/unit/test_encrypted_columns.py
+++ tests/unit/test_encrypted_columns.py
@@ -1,5 +1,7 @@
 import pytest  # noqa F401
 
+from sqlalchemy import text
+
 from broker.extensions import db
 from tests.lib.factories import (
     ACMEUserFactory,
@@ -12,9 +14,11 @@ def test_stores_acmeuser_private_key_pem_encrypted(client):
 
     si = ACMEUserFactory.create(private_key_pem="UNENCRYPTED")
     db.session.commit()
-    row = db.engine.execute(
-        f"select private_key_pem from acme_user where id='{si.id}'"
-    ).first()
+    with db.engine.connect() as conn:
+        row = conn.execute(
+            text(f"select private_key_pem from acme_user where id='{si.id}'")
+        ).first()
+    
     assert row
     assert row[0] != "UNENCRYPTED"
 
@@ -24,8 +28,10 @@ def test_stores_service_instance_private_key_pem_encrypted(client):
     si = ALBServiceInstanceFactory.create()
     cert = CertificateFactory.create(service_instance=si, private_key_pem="UNENCRYPTED")
     db.session.commit()
-    row = db.engine.execute(
-        f"select private_key_pem from certificate where id='{cert.id}'"
-    ).first()
+    with db.engine.connect() as conn:
+        row = conn.execute(
+            text(f"select private_key_pem from certificate where id='{cert.id}'")
+        ).first()
+    
     assert row
     assert row[0] != "UNENCRYPTED"
