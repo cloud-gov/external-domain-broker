157f0186eb2deb039a596653e3864bba3729e3c3
= <ben.berry@gsa.gov>
2024-04-17 17:10:49 -0700

    try to grab a new ALB when the existing ALB is full

--- tests/integration/dedicated_alb/test_dedicated_alb_pick_listener.py
+++ tests/integration/dedicated_alb/test_dedicated_alb_pick_listener.py
@@ -90,3 +90,44 @@ def test_selects_unassigned_alb(clean_db, alb, service_instance):
     assert service_instance.alb_arn.startswith("alb-our-arn-0")
     assert listener.dedicated_org == "our-org"
     assert listener.alb_arn.startswith("alb-our-arn-0")
+
+
+def test_adds_new_alb_when_assigned_is_near_full(clean_db, alb, service_instance):
+
+    our_listener_0 = DedicatedALBListener(
+        listener_arn="our-arn-0", dedicated_org="our-org"
+    )
+    available_listener_0 = DedicatedALBListener(listener_arn="available-arn-0")
+    other_listener_0 = DedicatedALBListener(
+        listener_arn="other-arn-0", dedicated_org="other-org"
+    )
+    other_listener_1 = DedicatedALBListener(
+        listener_arn="other-arn-1", dedicated_org="other-org"
+    )
+
+    clean_db.session.add_all(
+        [our_listener_0, available_listener_0, other_listener_0, other_listener_1]
+    )
+    clean_db.session.commit()
+    for i in range(17):
+        instance = factories.DedicatedALBServiceInstanceFactory.create(
+            domain_names=[f"random{i}.example.com"], alb_listener_arn="our-arn-0"
+        )
+        clean_db.session.add(instance)
+    clean_db.session.commit()
+
+    clean_db.session.expunge_all()
+    service_instance = clean_db.session.get(DedicatedALBServiceInstance, "4321")
+    alb.expect_get_certificates_for_listener("available-arn-0", 0)
+    alb.expect_get_listeners("available-arn-0")
+    get_lowest_dedicated_alb(service_instance, clean_db)
+    alb.assert_no_pending_responses()
+    clean_db.session.expunge_all()
+
+    listener = DedicatedALBListener.query.filter(
+        DedicatedALBListener.listener_arn == "available-arn-0"
+    ).first()
+    service_instance = clean_db.session.get(DedicatedALBServiceInstance, "4321")
+    assert service_instance.alb_arn.startswith("alb-available-arn-0")
+    assert listener.dedicated_org == "our-org"
+    assert listener.alb_arn.startswith("alb-available-arn-0")
