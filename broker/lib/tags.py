8b53e524ee7c67ecf75d0576663dc6142e18b7f2
Mark Boyd <mark.boyd@gsa.gov>
2024-07-02 10:56:38 -0400

    Update broker/lib/tags.py
    Co-authored-by: Ben Berry <ben.berry@gsa.gov>
    Signed-off-by: Mark Boyd <markdavidboyd@gmail.com>

--- broker/lib/tags.py
+++ broker/lib/tags.py
@@ -1,7 +1,7 @@
 def add_tag(tags, tag_key: str, tag_value: str):
     if not tags:
         tags = {}
-    tags["Items"] = tags["Items"] if "Items" in tags else []
+    tags["Items"] = tags.get("Items", [])
     tags["Items"].append(
         {
             "Key": tag_key,
