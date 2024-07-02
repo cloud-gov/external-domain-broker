d2966c3caee9e0f342cc684d956ccbd9be5c73e8
Mark Boyd <mark.boyd@gsa.gov>
2024-07-02 16:26:38 -0400

    remove unused line

--- broker/extensions.py
+++ broker/extensions.py
@@ -2,7 +2,6 @@ from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 
 from broker.config import config_from_env
-from broker.tasks.shield import ShieldProtections
 
 config = config_from_env()
 db = SQLAlchemy(disable_autonaming=True)
