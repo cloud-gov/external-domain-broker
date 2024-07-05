082c115c31e4cf727116415d0821d502cee040bb
Mark Boyd <mark.boyd@gsa.gov>
2024-07-05 12:12:11 -0400

    stub out method for adding tags

--- broker/lib/tags.py
+++ broker/lib/tags.py
@@ -1,3 +1,12 @@
+import typing
+
+from broker.extensions import config
+
+from openbrokerapi.service_broker import (
+    ProvisionDetails,
+)
+
+
 def add_tag(tags, tag_key: str, tag_value: str):
     if not tags:
         tags = {}
@@ -9,3 +18,17 @@ def add_tag(tags, tag_key: str, tag_value: str):
         }
     )
     return tags
+
+
+def generate_default_tags(
+    instance_id: str, details: ProvisionDetails
+) -> typing.Dict[str, str]:
+    default_tags = {
+        "client": "Cloud Foundry",
+        "broker": "External domain broker",
+        "environment": config.FLASK_ENV,
+        "Instance GUID": instance_id,
+        "Organization GUID": details.organization_guid,
+        "Space GUID": details.space_guid,
+    }
+    return default_tags
