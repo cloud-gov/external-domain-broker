8d137cb62d6798343993cb90921f89612df67f84
Mark Boyd <mark.boyd@gsa.gov>
2024-08-20 17:42:26 -0400

    rename migration file

--- migrations/versions/7641f0967110_add_tags_column.py
+++ migrations/versions/7641f0967110_add_tags_column.py
@@ -1,8 +1,8 @@
-"""add tags column
+"""add_tags_column
 
-Revision ID: 7ddef9f2b7a6
-Revises: 8bfa2dd3a367
-Create Date: 2024-07-05 18:48:55.778958
+Revision ID: 7641f0967110
+Revises: 504d3d141b25
+Create Date: 2024-08-20 21:37:26.698389
 
 """
 
@@ -11,8 +11,8 @@ import sqlalchemy as sa
 from sqlalchemy.dialects import postgresql
 
 # revision identifiers, used by Alembic.
-revision = "7ddef9f2b7a6"
-down_revision = "8bfa2dd3a367"
+revision = "7641f0967110"
+down_revision = "504d3d141b25"
 branch_labels = None
 depends_on = None
 
