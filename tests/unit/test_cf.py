7826a3ce3e79b0e332dea8734e5cf6cdac4ad04b
Mark Boyd <mark.boyd@gsa.gov>
2024-08-21 16:00:28 -0400

    update unit tests

--- tests/unit/test_cf.py
+++ tests/unit/test_cf.py
@@ -15,7 +15,12 @@ def organization_guid():
     return str(uuid.uuid4())
 
 
-def test_gets_space_name(space_guid):
+@pytest.fixture
+def access_token():
+    return str(uuid.uuid4())
+
+
+def test_gets_space_name(space_guid, access_token):
     with requests_mock.Mocker() as m:
         response = """
   {
@@ -26,11 +31,14 @@ def test_gets_space_name(space_guid):
         m.get(
             f"http://mock.cf/v3/spaces/{space_guid}",
             text=response,
+            request_headers={
+                "Authorization": f"Bearer {access_token}",
+            },
         )
-        assert cf.get_space_name_by_guid(space_guid, "a_token") == "foobar-space"
+        assert cf.get_space_name_by_guid(space_guid, access_token) == "foobar-space"
 
 
-def test_gets_org_name(organization_guid):
+def test_gets_org_name(organization_guid, access_token):
     with requests_mock.Mocker() as m:
         response = """
   {
@@ -41,5 +49,8 @@ def test_gets_org_name(organization_guid):
         m.get(
             f"http://mock.cf/v3/organizations/{organization_guid}",
             text=response,
+            request_headers={
+                "Authorization": f"Bearer {access_token}",
+            },
         )
-        assert cf.get_org_name_by_guid(organization_guid, "a_token") == "org-1234"
+        assert cf.get_org_name_by_guid(organization_guid, access_token) == "org-1234"
