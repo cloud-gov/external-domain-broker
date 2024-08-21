f278d470edb0b8cddc5866b1056053b47faeb00b
Mark Boyd <mark.boyd@gsa.gov>
2024-08-21 15:24:14 -0400

    add TODO

--- broker/tags.py
+++ broker/tags.py
@@ -3,15 +3,16 @@ import logging
 from sqlalchemy import select
 
 from broker.extensions import db
-from broker.models import CDNServiceInstance
+from broker.models import ServiceInstance
 
 logger = logging.getLogger(__name__)
 
 
-def find_cdn_instances_without_tags():
+# TODO: implement ability to find instances without tags and update them
+def find_instances_without_tags():
     query = (
-        select(CDNServiceInstance.id, CDNServiceInstance.cloudfront_distribution_arn)
-        .select_from(CDNServiceInstance)
-        .where(CDNServiceInstance.tags == None)
+        select(ServiceInstance.id)
+        .select_from(ServiceInstance)
+        .where(ServiceInstance.tags == None)
     )
     return db.session.execute(query).fetchall()
