f325c7aea061f0d642580a4fa07e22db31a6d2fc
Mark Boyd <mark.boyd@gsa.gov>
2024-08-22 10:25:23 -0400

    refactor CF API client and calls

--- broker/lib/cf.py
+++ broker/lib/cf.py
@@ -1,24 +1,57 @@
 import requests
+from functools import cache
+from time import time
 from urllib.parse import urljoin
 
 from broker.extensions import config
 
 
-def get_space_name_by_guid(space_guid, access_token):
-    with requests.Session() as s:
-        s.headers["Authorization"] = f"Bearer {access_token}"
-        url = urljoin(config.CF_API_URL, f"v3/spaces/{space_guid}")
-        response = s.get(url)
-        response.raise_for_status()
-        data = response.json()
-        return data["name"]
+class CFAPIClient:
+    _access_token: str
+    _expires_at: int
 
+    def __init__(self):
+        self.set_access_token()
 
-def get_org_name_by_guid(organization_guid, access_token):
-    with requests.Session() as s:
-        s.headers["Authorization"] = f"Bearer {access_token}"
-        url = urljoin(config.CF_API_URL, f"v3/organizations/{organization_guid}")
-        response = s.get(url)
-        response.raise_for_status()
-        data = response.json()
-        return data["name"]
+    def set_access_token(self):
+        if hasattr(self, "_access_token"):
+            return
+
+        r = requests.post(
+            config.UAA_TOKEN_URL,
+            data={
+                "grant_type": "client_credentials",
+            },
+            auth=requests.auth.HTTPBasicAuth(
+                config.UAA_CLIENT_ID, config.UAA_CLIENT_SECRET
+            ),
+            timeout=config.REQUEST_TIMEOUT,
+        )
+        r.raise_for_status()
+        response = r.json()
+        self._access_token = response["access_token"]
+        # expires_at = current time in seconds + number of seconds until token expires
+        self._expires_at = int(time()) + response["expires_in"]
+
+    def get_access_token(self):
+        return self._access_token
+
+    @cache
+    def get_space_name_by_guid(self, space_guid):
+        with requests.Session() as s:
+            s.headers["Authorization"] = f"Bearer {self.get_access_token()}"
+            url = urljoin(config.CF_API_URL, f"v3/spaces/{space_guid}")
+            response = s.get(url)
+            response.raise_for_status()
+            data = response.json()
+            return data["name"]
+
+    @cache
+    def get_org_name_by_guid(self, organization_guid):
+        with requests.Session() as s:
+            s.headers["Authorization"] = f"Bearer {self.get_access_token()}"
+            url = urljoin(config.CF_API_URL, f"v3/organizations/{organization_guid}")
+            response = s.get(url)
+            response.raise_for_status()
+            data = response.json()
+            return data["name"]
--- tests/unit/test_cf.py
+++ tests/unit/test_cf.py
@@ -1,9 +1,12 @@
 import pytest
+import base64
 import json
 import uuid
 import requests_mock
 
+from broker.extensions import config
 from broker.lib import cf
+from requests import Response, exceptions
 
 
 @pytest.fixture
@@ -21,7 +24,60 @@ def access_token():
     return str(uuid.uuid4())
 
 
-def test_gets_space_name(space_guid, access_token):
+@pytest.fixture
+def access_token_response(access_token):
+    return json.dumps({"access_token": access_token, "expires_in": 10})
+
+
+@pytest.fixture
+def cf_api_client(access_token_response):
+    with requests_mock.Mocker() as m:
+        m.post(
+            f"http://mock.uaa/token",
+            text=access_token_response,
+            additional_matcher=match_uaa_basic_auth,
+        )
+        return cf.CFAPIClient()
+
+
+def match_uaa_basic_auth(request):
+    basic_auth = bytes.decode(
+        base64.b64encode(
+            bytes(f"{config.UAA_CLIENT_ID}:{config.UAA_CLIENT_SECRET}", "utf-8")
+        )
+    )
+    if request.headers["Authorization"] == f"Basic {basic_auth}":
+        resp = Response()
+        resp.status_code = 200
+        return resp
+    return None
+
+
+def test_gets_access_token(access_token, access_token_response):
+    with requests_mock.Mocker() as m:
+        m.post(
+            f"http://mock.uaa/token",
+            text=access_token_response,
+            additional_matcher=match_uaa_basic_auth,
+        )
+
+        cfApiClient = cf.CFAPIClient()
+        assert cfApiClient.get_access_token() == access_token
+
+
+def test_get_access_token_error():
+    with requests_mock.Mocker() as m:
+        m.post(
+            f"http://mock.uaa/token",
+            status_code=500,
+            additional_matcher=match_uaa_basic_auth,
+        )
+
+        with pytest.raises(exceptions.HTTPError):
+            cf.CFAPIClient()
+
+
+def test_gets_space_name(space_guid, access_token, cf_api_client):
     with requests_mock.Mocker() as m:
         response = json.dumps({"guid": space_guid, "name": "foobar-space"})
         m.get(
@@ -31,10 +87,11 @@ def test_gets_space_name(space_guid, access_token):
                 "Authorization": f"Bearer {access_token}",
             },
         )
-        assert cf.get_space_name_by_guid(space_guid, access_token) == "foobar-space"
+
+        assert cf_api_client.get_space_name_by_guid(space_guid) == "foobar-space"
 
 
-def test_gets_space_name_error(space_guid, access_token):
+def test_gets_space_name_error(space_guid, access_token, cf_api_client):
     with requests_mock.Mocker() as m:
         m.get(
             f"http://mock.cf/v3/spaces/{space_guid}",
@@ -43,11 +100,11 @@ def test_gets_space_name_error(space_guid, access_token):
             },
             status_code=500,
         )
-        with pytest.raises(Exception):
-            cf.get_space_name_by_guid(space_guid, access_token)
+        with pytest.raises(exceptions.HTTPError):
+            cf_api_client.get_space_name_by_guid(space_guid)
 
 
-def test_gets_org_name(organization_guid, access_token):
+def test_gets_org_name(organization_guid, access_token, cf_api_client):
     with requests_mock.Mocker() as m:
         response = json.dumps({"guid": organization_guid, "name": "org-1234"})
         m.get(
@@ -57,10 +114,10 @@ def test_gets_org_name(organization_guid, access_token):
                 "Authorization": f"Bearer {access_token}",
             },
         )
-        assert cf.get_org_name_by_guid(organization_guid, access_token) == "org-1234"
+        assert cf_api_client.get_org_name_by_guid(organization_guid) == "org-1234"
 
 
-def test_gets_org_name_error(organization_guid, access_token):
+def test_gets_org_name_error(organization_guid, access_token, cf_api_client):
     with requests_mock.Mocker() as m:
         m.get(
             f"http://mock.cf/v3/organizations/{organization_guid}",
@@ -69,5 +126,5 @@ def test_gets_org_name_error(organization_guid, access_token):
             },
             status_code=500,
         )
-        with pytest.raises(Exception):
-            cf.get_org_name_by_guid(space_guid, access_token)
+        with pytest.raises(exceptions.HTTPError):
+            cf_api_client.get_org_name_by_guid(organization_guid)
