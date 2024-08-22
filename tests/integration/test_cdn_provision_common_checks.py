251b6a91a23a000b94c3670f5bd27bb26ad81a90
Mark Boyd <mark.boyd@gsa.gov>
2024-08-22 17:12:30 -0400

    fix integration tests

--- tests/integration/test_cdn_provision_common_checks.py
+++ tests/integration/test_cdn_provision_common_checks.py
@@ -8,15 +8,29 @@ from broker.models import (
 )
 
 
+# @pytest.fixture
+# def provisioned_instance_client(
+#     client, dns, organization_guid, space_guid, instance_model
+# ):
+
+#     yield client
+
+
 @pytest.mark.parametrize(
     "instance_model",
     [CDNServiceInstance, CDNDedicatedWAFServiceInstance],
 )
 def test_provision_sets_default_origin_and_path_if_none_provided(
-    instance_model, client, dns
+    dns, client, organization_guid, space_guid, instance_model
 ):
     dns.add_cname("_acme-challenge.example.com")
-    client.provision_instance(instance_model, "4321", params={"domains": "example.com"})
+    client.provision_instance(
+        instance_model,
+        "4321",
+        params={"domains": "example.com"},
+        organization_guid=organization_guid,
+        space_guid=space_guid,
+    )
     db.session.expunge_all()
 
     assert client.response.status_code == 202, client.response.body
@@ -32,10 +46,16 @@ def test_provision_sets_default_origin_and_path_if_none_provided(
     [CDNServiceInstance, CDNDedicatedWAFServiceInstance],
 )
 def test_provision_sets_default_cookie_policy_if_none_provided(
-    instance_model, client, dns
+    dns, client, organization_guid, space_guid, instance_model
 ):
     dns.add_cname("_acme-challenge.example.com")
-    client.provision_instance(instance_model, "4321", params={"domains": "example.com"})
+    client.provision_instance(
+        instance_model,
+        "4321",
+        params={"domains": "example.com"},
+        organization_guid=organization_guid,
+        space_guid=space_guid,
+    )
     instance = db.session.get(instance_model, "4321")
     assert instance.forward_cookie_policy == "all"
     assert instance.forwarded_cookies == []
@@ -45,10 +65,16 @@ def test_provision_sets_default_cookie_policy_if_none_provided(
     "instance_model",
     [CDNServiceInstance, CDNDedicatedWAFServiceInstance],
 )
-def test_provision_sets_none_cookie_policy(instance_model, client, dns):
+def test_provision_sets_none_cookie_policy(
+    dns, client, organization_guid, space_guid, instance_model
+):
     dns.add_cname("_acme-challenge.example.com")
     client.provision_instance(
-        instance_model, "4321", params={"domains": "example.com", "forward_cookies": ""}
+        instance_model,
+        "4321",
+        params={"domains": "example.com", "forward_cookies": ""},
+        organization_guid=organization_guid,
+        space_guid=space_guid,
     )
     instance = db.session.get(instance_model, "4321")
     assert instance.forward_cookie_policy == "none"
@@ -59,7 +85,9 @@ def test_provision_sets_none_cookie_policy(instance_model, client, dns):
     "instance_model",
     [CDNServiceInstance, CDNDedicatedWAFServiceInstance],
 )
-def test_provision_sets_forward_cookie_policy_with_cookies(instance_model, client, dns):
+def test_provision_sets_forward_cookie_policy_with_cookies(
+    dns, client, organization_guid, space_guid, instance_model
+):
     dns.add_cname("_acme-challenge.example.com")
     client.provision_instance(
         instance_model,
@@ -68,6 +96,8 @@ def test_provision_sets_forward_cookie_policy_with_cookies(instance_model, clien
             "domains": "example.com",
             "forward_cookies": "my_cookie , my_other_cookie",
         },
+        organization_guid=organization_guid,
+        space_guid=space_guid,
     )
     instance = db.session.get(instance_model, "4321")
     assert instance.forward_cookie_policy == "whitelist"
@@ -78,12 +108,16 @@ def test_provision_sets_forward_cookie_policy_with_cookies(instance_model, clien
     "instance_model",
     [CDNServiceInstance, CDNDedicatedWAFServiceInstance],
 )
-def test_provision_sets_forward_cookie_policy_with_star(instance_model, client, dns):
+def test_provision_sets_forward_cookie_policy_with_star(
+    dns, client, organization_guid, space_guid, instance_model
+):
     dns.add_cname("_acme-challenge.example.com")
     client.provision_instance(
         instance_model,
         "4321",
         params={"domains": "example.com", "forward_cookies": "*"},
+        organization_guid=organization_guid,
+        space_guid=space_guid,
     )
     instance = db.session.get(instance_model, "4321")
     assert instance.forward_cookie_policy == "all"
@@ -95,10 +129,16 @@ def test_provision_sets_forward_cookie_policy_with_star(instance_model, client, 
     [CDNServiceInstance, CDNDedicatedWAFServiceInstance],
 )
 def test_provision_sets_forward_headers_to_host_when_none_specified(
-    instance_model, client, dns
+    dns, client, organization_guid, space_guid, instance_model
 ):
     dns.add_cname("_acme-challenge.example.com")
-    client.provision_instance(instance_model, "4321", params={"domains": "example.com"})
+    client.provision_instance(
+        instance_model,
+        "4321",
+        params={"domains": "example.com"},
+        organization_guid=organization_guid,
+        space_guid=space_guid,
+    )
     instance = db.session.get(instance_model, "4321")
     assert instance.forwarded_headers == ["HOST"]
 
@@ -108,7 +148,7 @@ def test_provision_sets_forward_headers_to_host_when_none_specified(
     [CDNServiceInstance, CDNDedicatedWAFServiceInstance],
 )
 def test_provision_sets_forward_headers_plus_host_when_some_specified(
-    instance_model, client, dns
+    dns, client, organization_guid, space_guid, instance_model
 ):
     dns.add_cname("_acme-challenge.example.com")
     client.provision_instance(
@@ -118,6 +158,8 @@ def test_provision_sets_forward_headers_plus_host_when_some_specified(
             "domains": "example.com",
             "forward_headers": "x-my-header,x-your-header",
         },
+        organization_guid=organization_guid,
+        space_guid=space_guid,
     )
     instance = db.session.get(instance_model, "4321")
     assert sorted(instance.forwarded_headers) == sorted(
@@ -130,13 +172,15 @@ def test_provision_sets_forward_headers_plus_host_when_some_specified(
     [CDNServiceInstance, CDNDedicatedWAFServiceInstance],
 )
 def test_provision_does_not_set_host_header_when_using_custom_origin(
-    instance_model, client, dns
+    dns, client, organization_guid, space_guid, instance_model
 ):
     dns.add_cname("_acme-challenge.example.com")
     client.provision_instance(
         instance_model,
         "4321",
         params={"domains": "example.com", "origin": "my-origin.example.gov"},
+        organization_guid=organization_guid,
+        space_guid=space_guid,
     )
     instance = db.session.get(instance_model, "4321")
     assert instance.forwarded_headers == []
@@ -146,9 +190,17 @@ def test_provision_does_not_set_host_header_when_using_custom_origin(
     "instance_model",
     [CDNServiceInstance, CDNDedicatedWAFServiceInstance],
 )
-def test_provision_sets_https_only_by_default(instance_model, client, dns):
+def test_provision_sets_https_only_by_default(
+    dns, client, organization_guid, space_guid, instance_model
+):
     dns.add_cname("_acme-challenge.example.com")
-    client.provision_instance(instance_model, "4321", params={"domains": "example.com"})
+    client.provision_instance(
+        instance_model,
+        "4321",
+        params={"domains": "example.com"},
+        organization_guid=organization_guid,
+        space_guid=space_guid,
+    )
     instance = db.session.get(instance_model, "4321")
     assert instance.origin_protocol_policy == "https-only"
 
@@ -157,7 +209,9 @@ def test_provision_sets_https_only_by_default(instance_model, client, dns):
     "instance_model",
     [CDNServiceInstance, CDNDedicatedWAFServiceInstance],
 )
-def test_provision_sets_http_when_set(instance_model, client, dns):
+def test_provision_sets_http_when_set(
+    dns, client, organization_guid, space_guid, instance_model
+):
     dns.add_cname("_acme-challenge.example.com")
     client.provision_instance(
         instance_model,
@@ -167,6 +221,8 @@ def test_provision_sets_http_when_set(instance_model, client, dns):
             "origin": "origin.gov",
             "insecure_origin": True,
         },
+        organization_guid=organization_guid,
+        space_guid=space_guid,
     )
     instance = db.session.get(instance_model, "4321")
     assert instance.origin_protocol_policy == "http-only"
@@ -177,13 +233,15 @@ def test_provision_sets_http_when_set(instance_model, client, dns):
     [CDNServiceInstance, CDNDedicatedWAFServiceInstance],
 )
 def test_provision_refuses_insecure_origin_for_default_origin(
-    instance_model, client, dns
+    dns, client, organization_guid, space_guid, instance_model
 ):
     dns.add_cname("_acme-challenge.example.com")
     client.provision_instance(
         instance_model,
         "4321",
         params={"domains": "example.com", "insecure_origin": True},
+        organization_guid=organization_guid,
+        space_guid=space_guid,
     )
     desc = client.response.json.get("description")
     assert "insecure_origin" in desc
@@ -194,7 +252,9 @@ def test_provision_refuses_insecure_origin_for_default_origin(
     "instance_model",
     [CDNServiceInstance, CDNDedicatedWAFServiceInstance],
 )
-def test_provision_sets_custom_error_responses(instance_model, client, dns):
+def test_provision_sets_custom_error_responses(
+    dns, client, organization_guid, space_guid, instance_model
+):
     dns.add_cname("_acme-challenge.example.com")
     client.provision_instance(
         instance_model,
@@ -203,6 +263,8 @@ def test_provision_sets_custom_error_responses(instance_model, client, dns):
             "domains": "example.com",
             "error_responses": {"404": "/errors/404.html"},
         },
+        organization_guid=organization_guid,
+        space_guid=space_guid,
     )
     instance = db.session.get(instance_model, "4321")
     assert instance.error_responses["404"] == "/errors/404.html"
