751391dc9592996dbbe7358aaf05340aa80e4728
Mark Boyd <mark.boyd@gsa.gov>
2024-08-29 13:56:58 -0400

    fix route53 and shield health check update logic & integration tests

--- tests/integration/cdn_dedicated_waf/update.py
+++ tests/integration/cdn_dedicated_waf/update.py
@@ -73,6 +73,75 @@ def subtest_update_deletes_unused_health_checks(
     route53.assert_no_pending_responses()
 
 
+def subtest_update_creates_new_health_checks(
+    tasks, route53, instance_model, service_instance_id="4321"
+):
+    db.session.expunge_all()
+    service_instance = db.session.get(instance_model, service_instance_id)
+
+    route53.expect_create_health_check(service_instance.id, "bar.com", 0)
+    route53.expect_change_tags_for_resource("bar.com", service_instance.tags)
+
+    # delete_health_check = [
+    #     check
+    #     for check in service_instance.route53_health_checks
+    #     if check["domain_name"] == "example.com"
+    # ][0]
+    # route53.expect_delete_health_check(delete_health_check["health_check_id"])
+
+    tasks.run_queued_tasks_and_enqueue_dependents()
+
+    db.session.expunge_all()
+    service_instance = db.session.get(instance_model, service_instance_id)
+    assert sorted(
+        service_instance.route53_health_checks,
+        key=lambda check: check["domain_name"],
+    ) == [
+        {
+            "domain_name": "bar.com",
+            "health_check_id": "bar.com ID",
+        },
+        {
+            "domain_name": "example.com",
+            "health_check_id": "example.com ID",
+        },
+        {"domain_name": "foo.com", "health_check_id": "foo.com ID"},
+    ]
+
+    route53.assert_no_pending_responses()
+
+
+def subtest_update_deletes_unused_health_checks(
+    tasks, route53, instance_model, service_instance_id="4321"
+):
+    db.session.expunge_all()
+    service_instance = db.session.get(instance_model, service_instance_id)
+
+    delete_health_check = [
+        check
+        for check in service_instance.route53_health_checks
+        if check["domain_name"] == "example.com"
+    ][0]
+    route53.expect_delete_health_check(delete_health_check["health_check_id"])
+
+    tasks.run_queued_tasks_and_enqueue_dependents()
+
+    db.session.expunge_all()
+    service_instance = db.session.get(instance_model, service_instance_id)
+    assert sorted(
+        service_instance.route53_health_checks,
+        key=lambda check: check["domain_name"],
+    ) == [
+        {
+            "domain_name": "bar.com",
+            "health_check_id": "bar.com ID",
+        },
+        {"domain_name": "foo.com", "health_check_id": "foo.com ID"},
+    ]
+
+    route53.assert_no_pending_responses()
+
+
 def subtest_updates_health_checks_do_not_change(tasks, route53, instance_model):
     db.session.expunge_all()
     service_instance = db.session.get(instance_model, "4321")
