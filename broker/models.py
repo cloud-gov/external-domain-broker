d39f8f931391a5909976916dc8e3be6ba6ff5f2a
Mark Boyd <mark.boyd@gsa.gov>
2024-09-18 16:36:01 -0400

    add sns_notification_topic_arn field

--- broker/models.py
+++ broker/models.py
@@ -193,6 +193,7 @@ class CDNDedicatedWAFServiceInstance(CDNServiceInstance):
     shield_associated_health_check = mapped_column(postgresql.JSONB, default={})
     cloudwatch_health_check_alarms = mapped_column(postgresql.JSONB, default=[])
     alarm_notification_email = mapped_column(db.String)
+    sns_notification_topic_arn = mapped_column(db.String)
 
     __mapper_args__ = {
         "polymorphic_identity": ServiceInstanceTypes.CDN_DEDICATED_WAF.value
