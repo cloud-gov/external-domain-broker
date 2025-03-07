4512b3dbfd7d92dcdbeec3cfb185046fd8a9c2db
Mark Boyd <mark.boyd@gsa.gov>
2025-03-07 17:03:59 -0500

    update unit test

--- tests/integration/models/test_dedicated_alb_listener.py
+++ tests/integration/models/test_dedicated_alb_listener.py
@@ -12,21 +12,22 @@ def test_load_albs_on_startup(clean_db):
 def test_load_albs_on_startup_doesnt_modify_assigned_instances(clean_db):
     listeners = DedicatedALBListener.query.all()
     assert len(listeners) == 0
-    DedicatedALBListener.load_albs(["arn-1", "arn-2"])
+    DedicatedALBListener.load_albs({"arn-1": "org1", "arn-2": "org1"})
     listeners = DedicatedALBListener.query.all()
     assert len(listeners) == 2
-    dedicated_listener = DedicatedALBListener.query.filter(
-        DedicatedALBListener.listener_arn == "arn-1"
-    ).first()
-    dedicated_listener.dedicated_org = "me"
-    clean_db.session.add(dedicated_listener)
-    clean_db.session.commit()
-    clean_db.session.expunge_all()
 
-    DedicatedALBListener.load_albs(["arn-1", "arn-2", "arn-3"])
+    # dedicated_listener = DedicatedALBListener.query.filter(
+    #     DedicatedALBListener.listener_arn == "arn-1"
+    # ).first()
+    # dedicated_listener.dedicated_org = "me"
+    # clean_db.session.add(dedicated_listener)
+    # clean_db.session.commit()
+    # clean_db.session.expunge_all()
+
+    DedicatedALBListener.load_albs({"arn-1": "org1", "arn-2": "org2"})
     listeners = DedicatedALBListener.query.all()
     assert len(listeners) == 3
     dedicated_listener = DedicatedALBListener.query.filter(
-        DedicatedALBListener.listener_arn == "arn-1"
+        DedicatedALBListener.listener_arn == "arn-2"
     ).first()
-    assert dedicated_listener.dedicated_org == "me"
+    assert dedicated_listener.dedicated_org == "org1"
