ae2dc287763d6f1c35b86fcdb25f3f2ca91b837d
Mark Boyd <mark.boyd@gsa.gov>
2025-03-14 10:34:33 -0400

    update integration test for DedicatedALBListener.load_albs

--- tests/integration/models/test_dedicated_alb_listener.py
+++ tests/integration/models/test_dedicated_alb_listener.py
@@ -16,17 +16,9 @@ def test_load_albs_on_startup_doesnt_modify_assigned_instances(clean_db):
     listeners = DedicatedALBListener.query.all()
     assert len(listeners) == 2
 
-    # dedicated_listener = DedicatedALBListener.query.filter(
-    #     DedicatedALBListener.listener_arn == "arn-1"
-    # ).first()
-    # dedicated_listener.dedicated_org = "me"
-    # clean_db.session.add(dedicated_listener)
-    # clean_db.session.commit()
-    # clean_db.session.expunge_all()
-
     DedicatedALBListener.load_albs({"arn-1": "org1", "arn-2": "org2"})
     listeners = DedicatedALBListener.query.all()
-    assert len(listeners) == 3
+    assert len(listeners) == 2
     dedicated_listener = DedicatedALBListener.query.filter(
         DedicatedALBListener.listener_arn == "arn-2"
     ).first()
