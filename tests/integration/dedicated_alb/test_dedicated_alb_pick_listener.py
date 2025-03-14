cbd97314831641850de02c15c5aab9a74d894e9b
Mark Boyd <mark.boyd@gsa.gov>
2025-03-14 16:23:10 -0400

    update integration tests

--- tests/integration/dedicated_alb/test_dedicated_alb_pick_listener.py
+++ tests/integration/dedicated_alb/test_dedicated_alb_pick_listener.py
@@ -70,45 +70,13 @@ def test_selects_existing_dedicated_alb(clean_db, alb, service_instance):
     assert service_instance.alb_arn.startswith("alb-our-arn-0")
 
 
-def test_selects_unassigned_alb(clean_db, alb, service_instance):
-    our_listener_0 = DedicatedALBListener(listener_arn="our-arn-0")
-    our_listener_1 = DedicatedALBListener(listener_arn="our-arn-1")
-    other_listener_0 = DedicatedALBListener(
-        listener_arn="other-arn-0", dedicated_org="other-org"
-    )
-    other_listener_1 = DedicatedALBListener(
-        listener_arn="other-arn-1", dedicated_org="other-org"
-    )
-
-    clean_db.session.add_all(
-        [our_listener_0, our_listener_1, other_listener_0, other_listener_1]
-    )
-    clean_db.session.commit()
-
-    clean_db.session.expunge_all()
-    service_instance = clean_db.session.get(DedicatedALBServiceInstance, "4321")
-    alb.expect_get_certificates_for_listener("our-arn-0", 1)
-    alb.expect_get_certificates_for_listener("our-arn-1", 5)
-    alb.expect_get_listeners("our-arn-0")
-    get_lowest_dedicated_alb(service_instance, clean_db)
-    alb.assert_no_pending_responses()
-    clean_db.session.expunge_all()
-
-    listener = DedicatedALBListener.query.filter(
-        DedicatedALBListener.listener_arn == "our-arn-0"
-    ).first()
-    service_instance = clean_db.session.get(DedicatedALBServiceInstance, "4321")
-    assert service_instance.alb_arn.startswith("alb-our-arn-0")
-    assert listener.dedicated_org == "our-org"
-    assert listener.alb_arn.startswith("alb-our-arn-0")
-
-
 def test_adds_new_alb_when_assigned_is_near_full(clean_db, alb, service_instance):
-
     our_listener_0 = DedicatedALBListener(
         listener_arn="our-arn-0", dedicated_org="our-org"
     )
-    available_listener_0 = DedicatedALBListener(listener_arn="available-arn-0")
+    available_listener_0 = DedicatedALBListener(
+        listener_arn="available-arn-0", dedicated_org="our-org"
+    )
     other_listener_0 = DedicatedALBListener(
         listener_arn="other-arn-0", dedicated_org="other-org"
     )
