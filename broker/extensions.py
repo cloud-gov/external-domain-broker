398ef06fb5c4fdb4f5077a3522371c365c519389
Mark Boyd <mark.boyd@gsa.gov>
2025-04-24 17:00:45 -0400

    revert change to imports

--- broker/extensions.py
+++ broker/extensions.py
@@ -1,11 +1,8 @@
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 
-from broker.aws import cloudfront
 from broker.config import config_from_env
-from broker.lib.cache_policy_manager import CachePolicyManager
 
 config = config_from_env()
 db = SQLAlchemy(disable_autonaming=True)
 migrate = Migrate()
-cache_policy_manager = CachePolicyManager(cloudfront)
