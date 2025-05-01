d9e315f7b8d8783c3afdfb15de4a1d9ae4463ab8
Mark Boyd <mark.boyd@gsa.gov>
2025-05-01 11:06:49 -0400

    update unit tests for CachePolicyManager to use mocked policy names matching the policy names as returned by the CloudFront ListCachePolicies API

--- tests/unit/test_cache_policy_manager.py
+++ tests/unit/test_cache_policy_manager.py
@@ -5,47 +5,47 @@ from broker.lib.cache_policy_manager import CachePolicyManager
 
 
 def test_managed_cache_policies(cloudfront, cache_policy_id):
-    policies = [{"id": cache_policy_id, "name": "CachingDisabled"}]
+    policies = [{"id": cache_policy_id, "name": "Managed-CachingDisabled"}]
 
     cache_policy_manager = CachePolicyManager(cloudfront_svc)
     cloudfront.expect_list_cache_policies("managed", policies)
     assert cache_policy_manager.managed_policies == {
-        "CachingDisabled": cache_policy_id,
+        "Managed-CachingDisabled": cache_policy_id,
     }
 
 
 def test_managed_cache_policies_handles_paging(cloudfront, cache_policy_id):
-    policies = [{"id": cache_policy_id, "name": "CachingDisabled"}]
+    policies = [{"id": cache_policy_id, "name": "Managed-CachingDisabled"}]
     cloudfront.expect_list_cache_policies("managed", policies, next_marker="next")
 
     policy2_id = str(uuid.uuid4())
-    policies = [{"id": policy2_id, "name": "CachingOptimized"}]
+    policies = [{"id": policy2_id, "name": "Managed-CachingOptimized"}]
 
     cloudfront.expect_list_cache_policies("managed", policies, marker="next")
 
     cache_policy_manager = CachePolicyManager(cloudfront_svc)
 
     assert cache_policy_manager.managed_policies == {
-        "CachingDisabled": cache_policy_id,
-        "CachingOptimized": policy2_id,
+        "Managed-CachingDisabled": cache_policy_id,
+        "Managed-CachingOptimized": policy2_id,
     }
 
 
 def test_managed_cache_policies_ignores_unknown_policies(cloudfront, cache_policy_id):
     policies = [
-        {"id": cache_policy_id, "name": "CachingDisabled"},
+        {"id": cache_policy_id, "name": "Managed-CachingDisabled"},
         {"id": "id-1", "name": "FoobarPolicy"},
     ]
 
     cache_policy_manager = CachePolicyManager(cloudfront_svc)
     cloudfront.expect_list_cache_policies("managed", policies)
     assert cache_policy_manager.managed_policies == {
-        "CachingDisabled": cache_policy_id,
+        "Managed-CachingDisabled": cache_policy_id,
     }
 
 
 def test_managed_cache_policies_returns_saved_results(cloudfront, cache_policy_id):
-    policies = [{"id": cache_policy_id, "name": "CachingDisabled"}]
+    policies = [{"id": cache_policy_id, "name": "Managed-CachingDisabled"}]
 
     cache_policy_manager = CachePolicyManager(cloudfront_svc)
     cloudfront.expect_list_cache_policies("managed", policies)
@@ -53,15 +53,16 @@ def test_managed_cache_policies_returns_saved_results(cloudfront, cache_policy_i
     # second call should not cause an API request
     policies = cache_policy_manager.managed_policies
     assert policies == {
-        "CachingDisabled": cache_policy_id,
+        "Managed-CachingDisabled": cache_policy_id,
     }
 
 
 def test_get_managed_cache_policy_id(cloudfront, cache_policy_id):
-    policies = [{"id": cache_policy_id, "name": "CachingDisabled"}]
+    policies = [{"id": cache_policy_id, "name": "Managed-CachingDisabled"}]
 
     cache_policy_manager = CachePolicyManager(cloudfront_svc)
     cloudfront.expect_list_cache_policies("managed", policies)
     assert (
-        cache_policy_manager.get_managed_policy_id("CachingDisabled") == cache_policy_id
+        cache_policy_manager.get_managed_policy_id("Managed-CachingDisabled")
+        == cache_policy_id
     )
