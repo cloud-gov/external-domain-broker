9a9ec2c19f5336e2910a2876dd81d523992c84f7
Mark Boyd <mark.boyd@gsa.gov>
2025-07-09 15:46:55 -0400

    fix more unit tests

--- tests/unit/test_alb.py
+++ tests/unit/test_alb.py
@@ -12,7 +12,7 @@ from tests.lib import factories
 
 def test_gets_lowest_used_alb(alb):
     alb.expect_get_certificates_for_listener("listener-arn-0", 1)
-    alb.expect_get_listeners("listener-arn-0")
+    alb.expect_get_listeners("listener-arn-0", "alb-listener-arn-0")
     assert get_lowest_used_alb(["listener-arn-0"]) == (
         "alb-listener-arn-0",
         "listener-arn-0",
@@ -20,7 +20,7 @@ def test_gets_lowest_used_alb(alb):
 
     alb.expect_get_certificates_for_listener("listener-arn-0", 2)
     alb.expect_get_certificates_for_listener("listener-arn-1", 1)
-    alb.expect_get_listeners("listener-arn-1")
+    alb.expect_get_listeners("listener-arn-1", "alb-listener-arn-1")
     assert get_lowest_used_alb(["listener-arn-0", "listener-arn-1"]) == (
         "alb-listener-arn-1",
         "listener-arn-1",
@@ -28,7 +28,7 @@ def test_gets_lowest_used_alb(alb):
 
     alb.expect_get_certificates_for_listener("listener-arn-1", 1)
     alb.expect_get_certificates_for_listener("listener-arn-0", 2)
-    alb.expect_get_listeners("listener-arn-1")
+    alb.expect_get_listeners("listener-arn-1", "alb-listener-arn-1")
     assert get_lowest_used_alb(["listener-arn-1", "listener-arn-0"]) == (
         "alb-listener-arn-1",
         "listener-arn-1",
@@ -37,7 +37,7 @@ def test_gets_lowest_used_alb(alb):
     alb.expect_get_certificates_for_listener("listener-arn-1", 1)
     alb.expect_get_certificates_for_listener("listener-arn-2", 2)
     alb.expect_get_certificates_for_listener("listener-arn-0", 2)
-    alb.expect_get_listeners("listener-arn-1")
+    alb.expect_get_listeners("listener-arn-1", "alb-listener-arn-1")
     assert get_lowest_used_alb(
         ["listener-arn-1", "listener-arn-2", "listener-arn-0"]
     ) == ("alb-listener-arn-1", "listener-arn-1")
@@ -47,7 +47,7 @@ def test_gets_lowest_used_alb(alb):
     alb.expect_get_certificates_for_listener("listener-arn-2", 25)
     alb.expect_get_certificates_for_listener("listener-arn-3", 20)
     alb.expect_get_certificates_for_listener("listener-arn-4", 17)
-    alb.expect_get_listeners("listener-arn-1")
+    alb.expect_get_listeners("listener-arn-1", "alb-listener-arn-1")
     assert get_lowest_used_alb(
         [
             "listener-arn-0",
@@ -61,7 +61,7 @@ def test_gets_lowest_used_alb(alb):
     alb.expect_get_certificates_for_listener("listener-arn-0", 1)
     alb.expect_get_certificates_for_listener("listener-arn-1", 1)
     alb.expect_get_certificates_for_listener("listener-arn-2", 0)
-    alb.expect_get_listeners("listener-arn-2")
+    alb.expect_get_listeners("listener-arn-2", "alb-listener-arn-2")
     assert get_lowest_used_alb(
         ["listener-arn-0", "listener-arn-1", "listener-arn-2"]
     ) == (
@@ -85,14 +85,21 @@ def test_get_potential_listeners_with_listeners_for_instance_org(
     no_context_clean_db, no_context_app
 ):
     with no_context_app.app_context():
+        alb = factories.DedicatedALBFactory.create(
+            alb_arn="alb-1", dedicated_org="org-1"
+        )
         listener = factories.DedicatedALBListenerFactory.create(
-            id=100, listener_arn="listener-arn-0", dedicated_org="org-1"
+            id=100,
+            listener_arn="listener-arn-0",
+            dedicated_org="org-1",
+            alb_arn="alb-1",
         )
         service_instance = factories.DedicatedALBServiceInstanceFactory.create(
             id=str(uuid.uuid4()),
             org_id="org-1",
         )
 
+        no_context_clean_db.session.add(alb)
         no_context_clean_db.session.add(listener)
         no_context_clean_db.session.add(service_instance)
         no_context_clean_db.session.commit()
@@ -107,14 +114,21 @@ def test_get_potential_listeners_no_listeners_found(
     no_context_clean_db, no_context_app
 ):
     with no_context_app.app_context():
+        alb = factories.DedicatedALBFactory.create(
+            alb_arn="alb-1", dedicated_org="org-1"
+        )
         listener = factories.DedicatedALBListenerFactory.create(
-            id=100, listener_arn="listener-arn-0", dedicated_org="org-1"
+            id=100,
+            listener_arn="listener-arn-0",
+            dedicated_org="org-1",
+            alb_arn="alb-1",
         )
         service_instance = factories.DedicatedALBServiceInstanceFactory.create(
             id=str(uuid.uuid4()),
             org_id="org-2",  # no listeners available for this org
         )
 
+        no_context_clean_db.session.add(alb)
         no_context_clean_db.session.add(listener)
         no_context_clean_db.session.add(service_instance)
         no_context_clean_db.session.commit()
