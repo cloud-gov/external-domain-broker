cc799cb3a76228b2b3a9632ad809981e3f253fb0
Mark Boyd <mark.boyd@gsa.gov>
2025-08-29 16:43:09 -0400

    update unit tests for creating and associating WAF web ACLs for dedicated ALB instances

--- tests/integration/commands/test_waf.py
+++ tests/integration/commands/test_waf.py
@@ -1,9 +1,11 @@
 import pytest
 
 from broker.commands.waf import (
-    add_dedicated_alb_waf_web_acls,
+    create_dedicated_alb_waf_web_acls,
     wait_for_web_acl_to_exist,
+    associate_dedicated_alb_waf_web_acls,
 )
+from broker.tasks.waf import generate_web_acl_name
 from broker.aws import wafv2_govcloud as real_wafv2_govcloud
 from broker.extensions import config
 from broker.models import DedicatedALB
@@ -53,7 +55,7 @@ def test_wait_for_web_acl_to_exist_gives_up(
     clean_db.session.expunge_all()
 
 
-def test_add_dedicated_alb_waf_web_acls(
+def test_create_dedicated_alb_waf_web_acls(
     clean_db,
     dedicated_alb_id,
     dedicated_alb,
@@ -69,12 +71,57 @@ def test_add_dedicated_alb_waf_web_acls(
         waf_web_acl_arn,
         config.ALB_WAF_CLOUDWATCH_LOG_GROUP_ARN,
     )
+
+    create_dedicated_alb_waf_web_acls()
+
+    wafv2_govcloud.assert_no_pending_responses()
+
+    clean_db.session.expunge_all()
+
+    service_instance = clean_db.session.get(
+        DedicatedALB,
+        dedicated_alb_id,
+    )
+
+    assert service_instance.dedicated_waf_web_acl_arn
+    assert service_instance.dedicated_waf_web_acl_id
+    assert service_instance.dedicated_waf_web_acl_name
+
+
+def test_create_dedicated_alb_waf_web_acls_does_nothing(
+    clean_db,
+    dedicated_alb_id,
+    dedicated_alb,
+    wafv2_govcloud,
+):
+    dedicated_alb.dedicated_waf_web_acl_id = "1234"
+    dedicated_alb.dedicated_waf_web_acl_name = "1234-dedicated-waf"
+    dedicated_alb.dedicated_waf_web_acl_arn = "1234-dedicated-waf-arn"
+    dedicated_alb.dedicated_waf_associated = True
+
+    clean_db.session.add(dedicated_alb)
+    clean_db.session.commit()
+
+    create_dedicated_alb_waf_web_acls()
+
+    wafv2_govcloud.assert_no_pending_responses()
+
+
+def test_associate_dedicated_alb_waf_web_acls(
+    clean_db, dedicated_alb_id, dedicated_alb, wafv2_govcloud
+):
+    dedicated_alb.dedicated_waf_web_acl_id = "1234"
+    dedicated_alb.dedicated_waf_web_acl_name = "1234-dedicated-waf"
+    dedicated_alb.dedicated_waf_web_acl_arn = "1234-dedicated-waf-arn"
+    clean_db.session.add(dedicated_alb)
+    clean_db.session.commit()
+
     wafv2_govcloud.expect_alb_associate_web_acl(
-        waf_web_acl_arn,
+        dedicated_alb.dedicated_waf_web_acl_arn,
         dedicated_alb.alb_arn,
     )
 
-    add_dedicated_alb_waf_web_acls()
+    associate_dedicated_alb_waf_web_acls()
 
     wafv2_govcloud.assert_no_pending_responses()
 
@@ -91,20 +138,21 @@ def test_add_dedicated_alb_waf_web_acls(
     assert service_instance.dedicated_waf_associated == True
 
 
-def test_add_dedicated_alb_waf_web_acls_does_nothing(
-    clean_db,
-    dedicated_alb_id,
-    dedicated_alb,
-    wafv2_govcloud,
+def test_associate_dedicated_alb_does_nothing(
+    clean_db, dedicated_alb_id, dedicated_alb, wafv2_govcloud
 ):
-    dedicated_alb.dedicated_waf_web_acl_id = "1234"
-    dedicated_alb.dedicated_waf_web_acl_name = "1234-dedicated-waf"
-    dedicated_alb.dedicated_waf_web_acl_arn = "1234-dedicated-waf-arn"
-    dedicated_alb.dedicated_waf_associated = True
-
-    clean_db.session.add(dedicated_alb)
-    clean_db.session.commit()
-
-    add_dedicated_alb_waf_web_acls()
+    associate_dedicated_alb_waf_web_acls()
 
     wafv2_govcloud.assert_no_pending_responses()
+
+    clean_db.session.expunge_all()
+
+    service_instance = clean_db.session.get(
+        DedicatedALB,
+        dedicated_alb_id,
+    )
+
+    assert not service_instance.dedicated_waf_web_acl_arn
+    assert not service_instance.dedicated_waf_web_acl_id
+    assert not service_instance.dedicated_waf_web_acl_name
+    assert service_instance.dedicated_waf_associated == False
